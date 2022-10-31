a13560f118cdccddc5f206b95baaae74e43ebbd3
Flores-AM <agustinmatias62@gmail.com>
2022-10-31 12:07:29 -0300

    no se ya

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,18 @@
 const { src, dest, parallel } = require("gulp");
+const react = require("gulp-react");
 const cache = require("gulp-cache");
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp");
 const avif = require("gulp-avif");
+
+gulp.task("default", function () {
+  return gulp
+    .src("template.jsx")
+    .pipe(sourcemaps.init())
+    .pipe(react())
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("dist"));
+});
 
 function imagen(done) {
   const opciones = {
